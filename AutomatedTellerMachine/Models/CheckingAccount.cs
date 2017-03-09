1ec0f27fa843e9138af4340e78a5a622288afc0b
cetra12 <jorge.cuevas.92@gmail.com>
2017-03-09 14:51:52 -0600

    added Validations to the Checking Account Models

--- AutomatedTellerMachine/Models/CheckingAccount.cs
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -10,12 +10,17 @@ namespace AutomatedTellerMachine.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        //[StringLength(10, MinimumLength=6)]
+        [RegularExpression(@"\d{6,10}", ErrorMessage = "Account must be between 6 and 10 digits.")]
         [Display(Name = "Account")]
         public string AccountNumber { get; set; }
 
+        [Required]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Required]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
